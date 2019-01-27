da12de28d5be61bbac4588eead83cbc4ac1faca1
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-01-27 22:50:20 +0000

    updating

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-teste
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    // res.send('Welcome to Home Page');
+    res.render('homePage');
+});
+
+app.get('/houses', function(req, res) {
+    // res.send('Welcome to house search Page');
+    res.render('house-search');
+});
+
+app.get('/houses', function(req, res) {
+    res.render('search');
+});
+
+app.listen(3000);
