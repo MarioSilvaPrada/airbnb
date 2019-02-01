fbdde4269932c77ac81e95fd6962f8e15cfcbad2
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-02-01 20:20:57 +0000

    updating

--- app.js
+++ app.js
@@ -2,16 +2,46 @@ let express = require('express');
 let app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public/style'));
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    // res.send('Welcome to Home Page');
+const rome = [
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
+        'price': '169€'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Romantico Appartamento con Vista sulla Fontana',
+        'price': '79€'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Modern Apartment with View of Piazza Del Popolo',
+        'price': '89€'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': "Campo de' Fiori Deluxe Apartment",
+        'price': '149€'
+    }
+]
+
+app.get('/', function (req, res) {
     res.render('homePage');
 });
 
-app.get('/houses', function(req, res) {
-    // res.send('Welcome to house search Page');
-    res.render('house-search');
+// app.get('/houses', function(req, res) {
+//     res.render('house-search');
+// });
+
+app.get('/s/:location/all', function (req, res) {
+    let location = req.params.location;
+
+    if (location.toLowerCase() !== 'rome') {
+        res.send('We found no results for that query');
+    }
+    res.render('search', { location: location, data: rome });
 });
 
 // app.get('/houses', function(req, res) {
