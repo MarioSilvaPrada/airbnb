39ce04391be8dc0db43e9aa34b86162d2287b370
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-02-03 20:45:44 +0000

    updating airbnbV5

--- app.js
+++ app.js
@@ -17,60 +17,37 @@ mongoose.connection.once('open', function () {
     console.log('Connection error:', error);
 });
 
-let rome = new Location ({
+const romeArr = [
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
+        'price': '169'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Romantico Appartamento con Vista sulla Fontana',
+        'price': '79'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': 'Modern Apartment with View of Piazza Del Popolo',
+        'price': '89'
+    },
+    {
+        'url': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+        'title': "Campo de' Fiori Deluxe Apartment",
+        'price': '149'
+    }
+]
+
+let rome = new Location({
     name: 'Rome',
-    houses: [
-        {
-            'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-            'title': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
-            'price': '169'
-        },
-        {
-            'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-            'title': 'Romantico Appartamento con Vista sulla Fontana',
-            'price': '79'
-        },
-        {
-            'url': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-            'title': 'Modern Apartment with View of Piazza Del Popolo',
-            'price': '89'
-        },
-        {
-            'url': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-            'title': "Campo de' Fiori Deluxe Apartment",
-            'price': '149'
-        }
-    ]
+    houses: romeArr
 })
 
 rome.save();
+console.log(rome['houses'])
 
-let romeLocation = Location.findOne({name:'Rome'}).then((result) => {
-    console.log(result);
-})
-
-// const rome = [
-//     {
-//         'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-//         'title': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
-//         'price': '169'
-//     },
-//     {
-//         'url': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-//         'title': 'Romantico Appartamento con Vista sulla Fontana',
-//         'price': '79'
-//     },
-//     {
-//         'url': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-//         'title': 'Modern Apartment with View of Piazza Del Popolo',
-//         'price': '89'
-//     },
-//     {
-//         'url': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-//         'title': "Campo de' Fiori Deluxe Apartment",
-//         'price': '149'
-//     }
-// ]
 
 // HOME PAGE
 
@@ -86,7 +63,11 @@ app.get('/s/:location/all', function (req, res) {
     if (location.toLowerCase() !== 'rome') {
         res.send('We found no results for that query');
     }
-    res.render('search', { location: location, data: rome, navbar: 'white_navbar' });
+
+    Location.findOne({ name: 'Rome' }).then((result) => {
+        res.render('search', { location: location, data: result['houses'], navbar: 'white_navbar' });
+    });
+
 });
 
 // HOMES PAGE 
@@ -94,7 +75,9 @@ app.get('/s/:location/all', function (req, res) {
 app.get('/s/:location/homes', function (req, res) {
     let location = req.params.location;
 
-    res.render('homes', { location: location, data: rome, navbar: 'white_navbar' });
+    Location.findOne({ name: 'Rome' }).then((result) => {
+        res.render('homes', { location: location, data: result['houses'], navbar: 'white_navbar' });
+    });
 });
 
 // FORM PAGE
