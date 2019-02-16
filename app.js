3317b76cfd4d24e9e4202cc0ae94a18a6db3e087
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-02-16 14:40:42 +0000

    updating

--- app.js
+++ app.js
@@ -24,53 +24,53 @@ mongoose.connection.once('open', function () {
     console.log('Connection error:', error);
 });
 
-const romeArr = [
-    {
-        'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-        'name': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
-        'price': '169',
-        'rating': 5,
-        'beds': 3
-    },
-    {
-        'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-        'name': 'Romantico Appartamento con Vista sulla Fontana',
-        'price': '79',
-        'rating': 5,
-        'beds': 3
-    },
-    {
-        'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-        'name': 'Modern Apartment with View of Piazza Del Popolo',
-        'price': '89',
-        'rating': 5,
-        'beds': 3
-    },
-    {
-        'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
-        'name': "Campo de' Fiori Deluxe Apartment",
-        'price': '149',
-        'rating': 5,
-        'beds': 3
-    }
-];
+// const romeArr = [
+//     {
+//         'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/c9db035f-7239-4b52-a8fa-b3e8622c8171.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+//         'name': 'Splendido Loft Appartamento Vicino a Piazza Dei Spagna',
+//         'price': '169',
+//         'rating': 5,
+//         'beds': 3
+//     },
+//     {
+//         'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/c44a74c2-df6a-4e0b-8300-6b18ae6901cd.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+//         'name': 'Romantico Appartamento con Vista sulla Fontana',
+//         'price': '79',
+//         'rating': 5,
+//         'beds': 3
+//     },
+//     {
+//         'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/b8262e99-4f56-4c42-adc6-6f0a0d2a4d2d.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+//         'name': 'Modern Apartment with View of Piazza Del Popolo',
+//         'price': '89',
+//         'rating': 5,
+//         'beds': 3
+//     },
+//     {
+//         'main_image': 'https://a0.muscache.com/4ea/air/v2/pictures/ae327b04-5ed4-4017-84ba-4744ce5dc43e.jpg?t=r:w1200-h720-sfit,e:fjpg-c90',
+//         'name': "Campo de' Fiori Deluxe Apartment",
+//         'price': '149',
+//         'rating': 5,
+//         'beds': 3
+//     }
+// ];
 
-let locationHouse = new Location({
-    name: 'rome',
-    houses: []
-});
+// let locationHouse = new Location({
+//     name: 'rome',
+//     houses: []
+// });
 
-romeArr.forEach((home, i) => {
+// romeArr.forEach((home, i) => {
 
-    new Home(home).save().then((result) => {
-        locationHouse.houses.push(result)
+//     new Home(home).save().then((result) => {
+//         locationHouse.houses.push(result)
 
-        if(i === romeArr.length - 1){
-            locationHouse.save()
-        }
-    })
+//         if(i === romeArr.length - 1){
+//             locationHouse.save()
+//         }
+//     })
 
-})
+// })
 
 
 
